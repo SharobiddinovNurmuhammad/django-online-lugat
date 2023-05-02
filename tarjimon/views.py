19cdb785651570345442b214447f930beba8d960
Your Name <sharobiddinovnurmuhammad23@gmail.com>
2023-05-03 01:52:25 +0500

    first-commit

--- tarjimon/views.py
+++ tarjimon/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import Lugat
 # Create your views here.
 
 def dictionary(request):
-    return render(request, 'index.html', {})
+    soz = request.GET.get('q', '') 
+    if soz and soz != '':
+        natija = Lugat.objects.filter(inglizcha__contains=soz).all()[:3]
+        print(natija)
+    else:
+        natija = None
+    return render(request, 'index.html', {'q':soz, 'natija': natija})
